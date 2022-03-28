e2c2f5ec4563b2482f7a21462ce3e3d56f4215b5
Musratov <musratof@gmail.com>
2022-03-28 16:47:19 +0500

    bnnma

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import './App.scss';
 
 import Header from './Components/Header/Header'
 import Sidebar from './Components/Sidebar/sidebar'
+import HomeScreen from './Screens/homeScreen/homeScreen'
+
 function App() {
 	return (
 		<>
     <Header/>
     <div className='app_container'>
       <Sidebar/>
+      <Container fluid className='app__main'>
+        <HomeScreen/>
+      </Container>
     </div>
 		</>
 	);
---
+++ src/Screens/homeScreen/homeScreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Video from '../../Components/Video/video'
+import CategoriesBar from '../../Components/CategoriesBar/categories'
+
+const Homescreen = () => {
+    return (
+        <Container>
+            <CategoriesBar/>
+            <Row>
+                {[...new Array(20)].map(() => (
+                    <Col lg={3} md={4}>
+                        <Video/>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+}
+
+export default Homescreen;
