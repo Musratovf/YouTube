9fac11c586438eeff491dac28de128a78141720c
Musratov <musratof@gmail.com>
2022-03-31 12:46:01 +0500

    img post vide file added

--- src/Components/CategoriesBar/categories.jsx
+++ src/Components/CategoriesBar/categories.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
-import "./_categories.scss"
+import { useState } from 'react';
+import './_categories.scss';
+
+const keywords = [
+	'All',
+	'React js',
+	'Angular js',
+	'React Native',
+	'use of API',
+	'Redux',
+	'Music',
+	'Algoritm Art',
+	'Guitar',
+	'Bengali Songs',
+	'Coding',
+	'Cricket',
+	'Football',
+	'Barselona',
+	'Gatsby',
+	'Poor Coder',
+	'Shwetabh',
+];
 
 const Categories = () => {
-    return (
-        <div>
-            Bar
-        </div>
-    );
-}
+	const [activeElement, setActiveElement] = useState('All');
+    const handleClick =(value)=>{
+        setActiveElement(value)
+    }
+
+	return (
+		<div className='CategoriesBar'>
+			{keywords.map((value, i) => (
+				<span 
+                onClick={()=>handleClick(value)}
+                key={i}
+                className={activeElement === value ? ' active' : ''}
+                >
+                {value}
+                </span>
+			))}
+		</div>
+	);
+};
 
 export default Categories;
--- src/Components/Video/video.jsx
+++ src/Components/Video/video.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './_video.scss'
 
+import {AiFillEye} from "react-icons/ai"
+
 const Video = () => {
-    return (
-        <div>
-            Video
+    return <div className="video">
+        <div className="video__top">
+            <img src="https://i.ytimg.com/vi/1K2EbBZ_lxg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYp3Bsv_zv139gdphZ_PQbjdgYKw" alt="" />
+            <span>05:43</span>
         </div>
-    );
+        <div className="video__title">
+            Create app in 5 minutes #made by Chintu
+        </div>
+        <div className="video__details">
+            <span>
+            <AiFillEye/> 5m Views • 
+            </span>
+            <span> 5 days ago</span>
+        </div>
+        <div className="video__channel">
+            <img src="https://yt3.ggpht.com/ytc/AKedOLTpTPlRxrDfszPRShO1Emc4dTiWvBeWf91WZKVe=s88-c-k-c0x00ffffff-no-rj" alt="" />
+            <p>Rainbow Hat Jr</p>
+        </div>
+    </div>
 }
 
 export default Video;
